72249ee29275e556fbe6656d53f8ce53ec720228
Guilherme <contato@iugmali.com>
2016-05-14 22:42:20 +0000

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,71 +1,48 @@
 class UsersController < ApplicationController
 
-def new
-
-@user = User.new
-
-end
-
-def index
-
-@users = User.all
-
-end
-
-def create
-
-@user = User.new(user_params)
-
-if @user.save
-
-flash[:success] = "Welcome to the alpha blog #{@user.username}"
-
-redirect_to articles_path
-
-else
-
-render 'new'
-
-end
-
-end
-
-def edit
-
-@user = User.find(params[:id])
-
-end
-
-def update
-
-@user = User.find(params[:id])
-
-if @user.update(user_params)
-
-flash[:success] = "Your account was updated successfully"
-
-redirect_to articles_path
-
-else
-
-render 'edit'
-
-end
-
-end
-
-def show
-
-@user = User.find(params[:id])
-
-end
-
-private
-
-def user_params
-
-params.require(:user).permit(:username, :email, :password)
-
-end
+  before_action :set_user, only: [:edit, :update, :show]
+  
+  def new
+    @user = User.new
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
